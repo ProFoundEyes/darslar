a801a74647774f9c8c6b5e201b6423e1815cb414
ProFound <105831446+ProFoundEyes@users.noreply.github.com>
2022-12-20 22:40:56 +0500

    vue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lessons/',
+  base: '/darslar/',
   plugins: [vue()],
   resolve: {
     alias: {
